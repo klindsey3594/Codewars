41594aebfef780004cce47266e2da165771d2f30
klindsey3594 <162655323+klindsey3594@users.noreply.github.com>
2025-09-13 13:27:35 -0500

    Create RevString.java

---
+++ RevString.java
@@ -0,0 +1,6 @@
+public class Kata {
+    // Reverses the given string and returns the result
+    public static String solution(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+}
